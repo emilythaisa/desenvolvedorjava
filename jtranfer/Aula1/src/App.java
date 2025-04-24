c576c6f2418ae433ed7c13aa160f186fef295dd8
aluno <turmannoturno@gmail.com>
2025-04-24 09:35:43 -0300

    aprendendo git

---
+++ jtranfer/Aula1/src/App.java
@@ -0,0 +1,54 @@
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //declaração de variáveis
+        String nome; //referência
+        int opcaoTransporte; //camelCasing
+        String meioTransporte="";
+
+        //classe para teclado
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("==Pesquisa de Transporte Diário");
+        System.out.println("Digite seu nome");
+        nome = scanner.nextLine();
+        System.out.println(nome);
+        System.out.println("como você vai ao trabalho?");
+        // utf-8 - João
+        System.out.println("1-Metrô");
+        System.out.println("2- Ônibus");
+        System.out.println("3-Caminhada");
+        System.out.println("4-Uber");
+        System.out.println("5-Táxi");
+        System.out.println("6-Carro");
+        System.out.println("Escolha uma opção (1-6)");
+
+        //exercicio
+        //faça como eu fiz
+        //leia a opçãoTransporte e mostre a opção lida
+        //casting (conversão) - texto/número (String/int)
+        opcaoTransporte = scanner.nextInt();
+       
+        switch(opcaoTransporte){
+            case 1:
+                 meioTransporte="Metrô";break;
+            case 2:
+                meioTransporte="Ônibus";break;
+            case 3:
+                meioTransporte="Caminhada";break;
+            case 4:
+                meioTransporte="Uber";break;
+            case 5:
+                meioTransporte="Taxi";break;
+            case 6:
+                meioTransporte="Carro";break;
+
+        }
+        //Task de amanhã: salvar os dados no arquivo 
+        //Task no github (git e github)
+        System.out.println("Nome: "+nome+ ","+ "meio do Transporte: " +meioTransporte);
+    }
+}
